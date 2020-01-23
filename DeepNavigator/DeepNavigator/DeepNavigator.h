f3acb0be647cd28743a54375ece4ae7d021ad9e1
Qutaibah Essa <qutaibah.essa@hotmail.com>
2020-01-23 19:44:18 +0300

    renaming project

---
+++ DeepNavigator/DeepNavigator/DeepNavigator.h
@@ -0,0 +1,19 @@
+//
+//  DeepNavigator.h
+//  DeepNavigator
+//
+//  Created by Qutaibah Essa on 23/01/2020.
+//  Copyright © 2020 HungerStation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DeepNavigator.
+FOUNDATION_EXPORT double DeepNavigatorVersionNumber;
+
+//! Project version string for DeepNavigator.
+FOUNDATION_EXPORT const unsigned char DeepNavigatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeepNavigator/PublicHeader.h>
+
+
---
+++ DeepNavigator/DeepNavigator/DeeplinkSource.swift
@@ -0,0 +1,17 @@
+//
+//  DeeplinkSource.swift
+//  DeepNavigator
+//
+//  Created by Qutaibah Essa on 15/09/2019.
+//  Copyright © 2019 com. All rights reserved.
+//
+
+import Foundation
+
+public enum DeeplinkSource: CaseIterable {
+    case launch
+    case silentNotification
+    case inAppNotificationClick
+    case openUrl
+    case inAppRouting
+}
