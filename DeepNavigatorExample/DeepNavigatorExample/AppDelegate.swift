a9826d9093ef8e7630f76b937cf0b6e6b03c0e6d
Qutaibah Essa <qutaibah.essa@hotmail.com>
2020-01-23 20:04:19 +0300

    implementing the example

--- DeepNavigatorExample/DeepNavigatorExample/AppDelegate.swift
+++ DeepNavigatorExample/DeepNavigatorExample/AppDelegate.swift
@@ -7,15 +7,45 @@
 //
 
 import UIKit
+import DeepNavigator
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    var delegates: [UIApplicationDelegate] = [DeeplinkKit.applicationDelegate]
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        for delegate in delegates {
+            let _ = delegate.application?(application, didFinishLaunchingWithOptions: launchOptions)
+        }
         return true
+    }
+
+            
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        for delegate in delegates {
+            delegate.application?(application, didReceiveRemoteNotification: userInfo, fetchCompletionHandler: completionHandler)
+        }
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        for delegate in delegates {
+            let _ = delegate.application?(app, open: url, options: options)
+        }
+        return true
+    }
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        for delegate in delegates {
+            let _ = delegate.application?(application, continue: userActivity, restorationHandler: restorationHandler)
+        }
+        return false
+    }
+    
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        print(shortcutItem.type)
     }
 }
 
